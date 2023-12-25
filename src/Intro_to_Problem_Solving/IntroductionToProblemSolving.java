ade96cbdb93c085fc8eea2a3cd65058c0ab5dfef
Premjeet Sahu <cs2054@global.org.in>
2023-12-25 11:41:41 +0530

    Day 1

--- src/Intro_to_Problem_Solving/IntroductionToProblemSolving.java
+++ src/Intro_to_Problem_Solving/IntroductionToProblemSolving.java
@@ -2,6 +2,21 @@ package Intro_to_Problem_Solving;
 
 public class IntroductionToProblemSolving {
     public static void main(String[] args) {
-
+        System.out.println(IsPrime(5));
     }
+
+    //Time Complexity O(√N)
+    // Space Complexity O(1)
+    public static boolean IsPrime(int n)
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
