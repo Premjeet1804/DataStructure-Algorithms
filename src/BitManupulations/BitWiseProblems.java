7ff33ebb7da1c25f30ffdcc7fac795560f90a8b8
Premjeet Sahu <cs2054@global.org.in>
2023-12-28 23:03:36 +0530

    Day 4

--- src/BitManupulations/BitWiseProblems.java
+++ src/BitManupulations/BitWiseProblems.java
@@ -1,9 +1,12 @@
 package BitManupulations;
 
+import java.util.*;
+
 public class BitWiseProblems {
     public static void main(String[] args) {
         System.out.println(singleNumber(new int[]{1,2,2,1,3,3,4
         }));
+        System.out.println(numSetBits(3));
     }
     //Given an array of integers A, every element appears twice except for one.
     // Find that integer that occurs once.
@@ -16,4 +19,22 @@ public class BitWiseProblems {
         }
         return a;
     }
+
+//    Write a function that takes an integer and returns the number of 1 bit
+//    present in its binary representation.
+    public static int numSetBits(int A) {
+        int count=0;
+        while(A>0)
+        {
+            if((A&1) == 1)
+            {
+                count++;
+            }
+            A = A>>1;
+        }
+        return count;
+
+    }
+
+
 }
