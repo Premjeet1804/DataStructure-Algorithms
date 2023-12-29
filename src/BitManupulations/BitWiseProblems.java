9b2786a8b96cf42ec88b177d30153bb52e14d409
Premjeet Sahu <cs2054@global.org.in>
2023-12-29 22:08:50 +0530

    Day 5

--- src/BitManupulations/BitWiseProblems.java
+++ src/BitManupulations/BitWiseProblems.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public class BitWiseProblems {
     public static void main(String[] args) {
+
         System.out.println(singleNumber(new int[]{1,2,2,1,3,3,4
         }));
-        System.out.println(numSetBits(3));
+
     }
     //Given an array of integers A, every element appears twice except for one.
     // Find that integer that occurs once.
@@ -34,6 +35,25 @@ public class BitWiseProblems {
         }
         return count;
 
+    }
+//    Given two binary strings A and B. Return their sum (also a binary string).
+
+    public static String addBinary(String A, String B) {
+        int i = A.length()-1;
+        int j = B.length()-1;
+        int carry = 0;
+        StringBuilder res = new StringBuilder();
+        while(i>=0 || j>=0)
+        {
+            int sum=carry;
+            if(i>=0) sum+=A.charAt(i--)-'0';
+            if(j>=0) sum+=B.charAt(j--)-'0';
+
+            res.append(sum%2);
+            carry = sum>1?1:0;
+        }
+        if(carry!=0) res.append(carry);
+        return res.reverse().toString();
     }
 
 
